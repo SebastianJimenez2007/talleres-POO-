868dcf934673439818ccd4f245e8a3f378641c18
Sebastian Jimenez <168003614+SebastianJimenez2007@users.noreply.github.com>
2025-02-17 23:40:47 -0500

    Add files via upload

---
+++ Producto.java
@@ -0,0 +1,17 @@
+public class Producto {
+    String nombre;
+    double precio;
+    int stock;
+
+    Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Stock disponible: " + stock);
+    }
+}
